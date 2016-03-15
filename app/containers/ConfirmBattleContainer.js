46547d8a121eed259a1bebd5f02eb412eb5ff00e
Weinan Qiu <davidiamyou@gmail.com>
2016-03-15 13:43:17 -0400

    async/await

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -12,14 +12,18 @@ const ConfirmBattleContainer = React.createClass({
       playersInfo: []
     }
   },
-  componentDidMount() {
+  async componentDidMount() {
     const { query } = this.props.location;
-    getPlayersInfo([query.playerOne, query.playerTwo]).then((players) => {
+    
+    try {
+      const players = await getPlayersInfo([query.playerOne, query.playerTwo])
       this.setState({
         isLoading: false,
         playersInfo: [players[0], players[1]]
-      });
-    })
+      })
+    } catch (error) {
+      console.warn('Error in ConfirmBattleContainer', error)
+    }
   },
   handleInitiateBattle() {
     this.context.router.push({
